e9c2f5da43437fac6b754557a7863a8533b2118e
gamemakerKOR <102915477+gamemakerKOR@users.noreply.github.com>
2025-12-01 17:24:21 +0900

    무한맵, 따라오는 적구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3f;
+    public Vector2 inputVec;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed",  inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+}
